1b782b73e373f68781dd63ddd74894ad83fb15ee
buyuknomad <54726967+buyuknomad@users.noreply.github.com>
2025-05-21 17:03:26 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ function App() {
               } />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/signup" element={<SignupPage />} />
+              <Route path="/signup-success" element={<SignupSuccessPage />} />
               
               {/* Protected routes */}
               <Route element={<ProtectedRoute />}>
