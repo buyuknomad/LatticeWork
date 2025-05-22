81d433f8de5f9f8abb8ae3147898d8f9960a7285
buyuknomad <54726967+buyuknomad@users.noreply.github.com>
2025-05-22 19:37:37 +0200

    Updated cn.ts

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,1 +1,8 @@
-aaa
+import { clsx, type ClassValue } from 'clsx';
+
+export function cn(...inputs: ClassValue[]) {
+  return clsx(inputs);
+}
+
+// Re-export clsx for direct usage
+export { clsx };
