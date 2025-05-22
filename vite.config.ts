42b576e7545419559a489a42ea76a7d32c02c8e4
buyuknomad <54726967+buyuknomad@users.noreply.github.com>
2025-05-22 18:48:53 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite" // Import from the new package
+import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // Use the plugin
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
