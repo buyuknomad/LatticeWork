bec41383709cc46ea6677e27a5e413f5cd164cce
buyuknomad <54726967+buyuknomad@users.noreply.github.com>
2025-05-29 11:10:03 +0200

    Deploy application to Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import BackgroundAnimation from './components/BackgroundAnimation';
 import FAQ from './pages/FAQ';
 import CheckoutSuccess from './pages/CheckoutSuccess';
 import ConfirmEmail from './pages/ConfirmEmail';
+import ScrollToTop from './components/ScrollToTop'; 
 
 // Import new pages
 import Terms from './pages/Terms';
@@ -104,6 +105,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <AuthProvider>
+        <ScrollToTop /> {/* Add the ScrollToTop component here */}
         <AppContent />
       </AuthProvider>
     </Router>
