1bdcc8a910550b0d62eb196096f2a0ef55e1cf7e
buyuknomad <54726967+buyuknomad@users.noreply.github.com>
2025-06-11 00:49:03 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ const AppContent: React.FC = () => {
             <Route path="/history" element={<History />} />
             <Route path="/dashboard/results" element={<Dashboard />} />
             <Route path="/dashboard-test" element={<ProtectedRoute><DashboardTest /></ProtectedRoute>} />
-<Route path="/dashboard-test/results" element={<ProtectedRoute><DashboardTest /></ProtectedRoute>} />
+            <Route path="/dashboard-test/results" element={<ProtectedRoute><DashboardTest /></ProtectedRoute>} />
             {/* Add other protected routes here */}
           </Route>
         </Routes>
