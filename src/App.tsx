a3c31aa5652664e68ef6b53e8d2ce2efbf6caf1a
buyuknomad <54726967+buyuknomad@users.noreply.github.com>
2025-06-11 23:27:49 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ const AppContent: React.FC = () => {
           <Route path="/signup-success" element={<SignupSuccessPage />} />
           <Route path="/checkout/success" element={<CheckoutSuccess />} />
           <Route path="/auth/confirm" element={<ConfirmEmail />} />
-   />
+  
           
           {/* Password Reset Routes */}
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
@@ -90,7 +90,7 @@ const AppContent: React.FC = () => {
             <Route path="/dashboard/results" element={<Dashboard />} />
                                {/* Test Dashboard Routes - Fixed */}
             <Route path="/dashboard-test" element={<DashboardTest />} />
-            <Route path="/dashboard-test/results" element={<DashboardTest />} 
+            <Route path="/dashboard-test/results" element={<DashboardTest />} />
 
         
             {/* Add other protected routes here */}
