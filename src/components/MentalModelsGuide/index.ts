9db4994632ff72ae75546ce77cf6a3aaf645acfe
buyuknomad <54726967+buyuknomad@users.noreply.github.com>
2025-08-04 10:38:08 +0200

    Updated index.ts

---
+++ src/components/MentalModelsGuide/index.ts
@@ -0,0 +1,9 @@
+// src/components/MentalModelsGuide/index.ts
+
+// Export all mental models guide components
+export { default as WhatAreMentalModels } from './WhatAreMentalModels';
+export { default as BenefitsSection } from './BenefitsSection';
+export { default as RealWorldExamples } from './RealWorldExamples';
+export { default as CategoryOverview } from './CategoryOverview';
+export { default as FeaturedModelsShowcase } from './FeaturedModelsShowcase';
+export { default as GetStartedCTA } from './GetStartedCTA';
